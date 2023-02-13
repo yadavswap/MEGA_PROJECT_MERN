b64021dd4b29650cafc28b215073cfb0c5a2328e
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2023-02-13 20:39:30 +0530

    user schema

---
+++ models/userSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+import AuthRoles from "../utils/authRoles"
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        maxLength: [50, "Name must be less than 50"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Pasword is required"],
+        minLength: [8, "Pasword must at least  be less than 8 characters"],
+        select: false
+
+    },
+    role: {
+        type: String,
+        enum: Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: String
+}, 
+{
+    timestamps: true
+})
+
+export default mongoose.model("User",userSchema)
