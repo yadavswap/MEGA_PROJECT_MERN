0b4be3d9beb908e0549ce6d8d0ee9a728aa8823e
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2023-02-13 21:54:08 +0530

    product schema added

---
+++ models/productSchema.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: [true, "Please provide a product  name"],
+            trim: true,
+            maxLength: [120, "Product name should not be more than 120 characters"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Please provide a product price"],
+            trim: true,
+            maxLength: [5, "Product price should not be more than 5 digits"]
+        },
+        description: {
+            type: String,
+            // use some form of editor - personal assignment
+        },
+        photos:[
+            {
+                secure_url:{
+                    type:String,
+                    required:true
+                }
+            }
+        ],
+        stock:{
+            type:Number,
+            default:0
+        },
+        collectionId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Collection"
+        }
+    }, {
+        timestamps: true
+    }
+
+);
+
+export default mongoose.model("Product",productSchema)
--- models/userSchema.js
+++ models/userSchema.js
@@ -29,9 +29,11 @@ const userSchema = mongoose.Schema({
     },
     forgotPasswordToken: String,
     forgotPasswordExpiry: Date,
-}, {
+}, 
+{
     timestamps: true
-})
+}
+)
 
 // challenge 1 - encrypt password
 userSchema.pre("save", async function (next) {
@@ -54,12 +56,26 @@ userSchema.methods = {
                 _id: this._id,
                 role: this.role
             },
-            config.JWT_SECRET,
-             {
+            config.JWT_SECRET, {
                 expiresIn: config.JWT_EXPIRY
 
             }
         )
+    },
+
+    generateForgotPasswordToken:function(){
+        const forgotToken = crypto.randomBytes(20).toString('hex');
+
+        // step 1 - save to DB
+        this.forgotPasswordToken = crypto
+        .createHash("sha256")
+        .update(forgotToken)
+        .digest("hex")
+
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 *1000
+        // step2 - return values to user
+
+        return forgotToken
     }
 }
 
