fef37c28aacbe033eb7d243570ec7950fdfd1df9
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2023-02-14 18:29:27 +0530

    profile api

--- controllers/authController.js
+++ controllers/authController.js
@@ -137,29 +137,31 @@ export const forgotPassword = asyncHandler(async (req, res) => {
     }
 
     const resetToken = user.generateForgotPasswordToken()
-    await user.save({validateBeforeSave:false})
-    const resetUrl = 
-    `${req.protocol}://${req.get("host")}/api/auth/password/reset${resetToken}`
+    await user.save({
+        validateBeforeSave: false
+    })
+    const resetUrl =
+        `${req.protocol}://${req.get("host")}/api/auth/password/reset${resetToken}`
     const text = `Your password reset links is
         \n\n ${resetUrl} \n\n   
     `
     try {
         await mailHelper({
-            email:user.email,
-            subject:"Password reset email for website",
-            text:text,
+            email: user.email,
+            subject: "Password reset email for website",
+            text: text,
         })
         res.status(200).json({
-            success:true,
-            message:`Email send to ${user.email}`
+            success: true,
+            message: `Email send to ${user.email}`
         })
     } catch (error) {
         // roll back - clear fields and save
 
         user.forgotPasswordToken = undefined
-        user.forgotPasswordExpiry=undefined
-        throw new CustomError(error.message || 'Email send failure',500)
-        
+        user.forgotPasswordExpiry = undefined
+        throw new CustomError(error.message || 'Email send failure', 500)
+
     }
 
 })
@@ -175,40 +177,70 @@ export const forgotPassword = asyncHandler(async (req, res) => {
 
 export const resetPassword = asyncHandler(async (req, res) => {
 
-    const {token:resetToken}= req.params
-    const {password,confirmPassword} = req.body
+    const {
+        token: resetToken
+    } = req.params
+    const {
+        password,
+        confirmPassword
+    } = req.body
     const resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex')
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
 
-  const   User.findOne({
-        forgotPasswordToken:resetPasswordToken,
-        forgotPasswordExpiry:{$gt:Date.now()}
+    const User.findOne({
+        forgotPasswordToken: resetPasswordToken,
+        forgotPasswordExpiry: {
+            $gt: Date.now()
+        }
     })
 
-    if(!user){
-        throw new CustomError('Password token is invalid or expired',400)
+    if (!user) {
+        throw new CustomError('Password token is invalid or expired', 400)
     }
-    if(password !== confirmPassword){
-        throw new CustomError('Password and conf password does not match',400)  
-      }
-      user.password = password
-      user.forgotPasswordToken=undefined
-      user.forgotPasswordExpiry=undefined
+    if (password !== confirmPassword) {
+        throw new CustomError('Password and conf password does not match', 400)
+    }
+    user.password = password
+    user.forgotPasswordToken = undefined
+    user.forgotPasswordExpiry = undefined
 
-      await user.save()
+    await user.save()
 
     //   create token  and send as response
     const token = user.getJwtToken()
     user.password = undefined
     // helper method for cookie cand be added
-    res.cookie('token',token,cookieOptions)
+    res.cookie('token', token, cookieOptions)
     res.status(200).json({
-        success:true,
+        success: true,
         user
     })
 })
 
 // TODO:create a controller for change password
 
+
+
+
+
+/*
+    @GET PRFOILE
+    @route http://localhost:5000/api/auth/profile
+    @description check token and populate req.user
+    @parameters  
+    @returns User Object
+
+*/
+
+export const getProfile = asyncHandler(async (req, res) => {
+    const {user} = req
+    if(!user){
+        throw new CustomError('User not found',404)
+    }
+    res.status(200).json({
+        success:true,
+        user
+    })
+})
