6ef476d33373452a206fa84048cec4a69d26e1c1
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2023-02-14 13:18:42 +0530

    auth controller signup

---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+import user from "../models/userSchema"
+import asyncHandler from "../services/asyncHandler"
+import CustomError from "../utils/customError"
+
+
+export const cookieOptions = {
+
+}
+
+/*
+    @SIGNUP
+    @route http://localhost:5000/api/auth/signup
+    @description user signUp Controller for creating new user
+    @parameters name,email,password
+    @returns User Object
+*/
+
+export const signUp = asyncHandler(async (req, res) => {
+    const {
+        name,
+        email,
+        password
+    } = req.body
+    if (!name || !email || !password) {
+        throw new CustomError("Please fill all fields",400)
+    }
+    // check if user exists
+    const existingUser = await User.findOne({email})
+    if(existingUser){
+        throw new CustomError('User alreay exits',400)
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    const token = user.getJwtToken()
+    console.log(user);
+    user.password = undefined
+
+    res.cookie("token",token,cookieOptions)
+    res.status(200).json({
+        success:true,
+        token,
+        user
+    })
+
+})
